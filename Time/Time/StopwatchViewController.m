64c7ceb2db2181206b84aea673f214dc0407b9ac
Daniel Distant <daniel.distant1@gmail.com>
2015-08-22 15:16:43 -0400

    Added more stopwatchTimer logic, stop button not completely done

--- Time/Time/StopwatchViewController.m
+++ Time/Time/StopwatchViewController.m
@@ -13,6 +13,8 @@
 @property (weak, nonatomic) IBOutlet UIButton *startButton;
 @property (weak, nonatomic) IBOutlet UIButton *lapButton;
 @property (weak, nonatomic) IBOutlet UITableView *lapTableView;
+@property (nonatomic) BOOL timerRunning;
+@property (nonatomic) NSTimer *stopwatchTimer;
 
 @end
 
@@ -21,17 +23,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
+    self.timerRunning = NO;
+    
 }
 - (IBAction)startButtonTapped:(id)sender {
-    NSTimer *stopwatchTimer = [NSTimer scheduledTimerWithTimeInterval:0.001 target:self selector:@selector(startButtonTapped:) userInfo:nil repeats:YES];
     
-    NSInteger *currentNumber = [self.stopwatchLabel.text integerValue];
-    NSInteger *nextNumber = currentNumber + 1;
+    if (self.timerRunning) {
+        // stop the timer
+        //update button to say start
+    } else {
+        
+        //start timer
+        //update button to say start
+    }
+    
+    [self.startButton setTitle:@"Stop" forState:UIControlStateNormal];
+    self.timerRunning = !self.timerRunning;
+    
+    NSTimer *stopwatchTimer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(startButtonTapped:) userInfo:nil repeats:YES];
+    
+    NSInteger currentNumber = [self.stopwatchLabel.text integerValue];
+    NSInteger nextNumber = currentNumber + 1;
     
     self.stopwatchLabel.text = [NSString stringWithFormat:@"%lu", nextNumber];
     
     [[NSRunLoop currentRunLoop] addTimer:stopwatchTimer forMode: NSDefaultRunLoopMode];
+    
+    NSLog(@"%@", self.startButton.currentTitle);
+    
+    if (self.timerRunning) {
+        
+        [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
+        [stopwatchTimer invalidate];
+        
+    }
 }
+
 - (IBAction)lapButtonTapped:(id)sender {
 }
 
