7b5801716fb68467c8a9735dbcbc154a0e02e6bf
Daniel Distant <daniel.distant1@gmail.com>
2015-08-22 11:15:35 -0400

    First commit, created tabs for tabBarController and 3 stopwatch, timer and settings viewcontrollers

---
+++ Time/Time/SettingsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsTableViewController.h
+//  Time
+//
+//  Created by Daniel Distant on 8/22/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsTableViewController : UITableViewController
+
+@end
---
+++ Time/Time/TabBarController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarController.h
+//  Time
+//
+//  Created by Daniel Distant on 8/22/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarController : UITabBarController
+
+@end
